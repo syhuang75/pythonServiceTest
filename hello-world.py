2970ac9aa9a33f9c693ab5350b61c18a2f251895
WINDOWS-P8I2MO6\syhuang <syhuang@cht.com.tw>
2023-02-23 14:51:51 +0800

    Change to debug mode and remove port setting.

--- hello-world.py
+++ hello-world.py
@@ -8,4 +8,5 @@ def hello():
     return {"name":"SyOneitem2","description":"這是一個測試用的NFT，採用remix測試，檔案皆放在IPFS上，僅供測試。","image":"ipfs://Qmejnv4RrwviMaTA6LzWeNiWDTJ1y4e2Xm8N46BDYXvQGf","external_link":"https://www.google.com","attributes":[{"trait_type":"顏色","value":"綠"},{"trait_type":"文字","value":"SyOne"}]}
 
 if __name__ == '__main__':
-    app.run(port=8088)
+    app.debug = True
+    app.run()
