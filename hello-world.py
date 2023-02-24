49fd0b83cd8fc7cfb38032fd935234e92a5d0799
WINDOWS-P8I2MO6\syhuang <syhuang@cht.com.tw>
2023-02-24 10:16:56 +0800

    Update code.

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,10 @@
+#run.py
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
+app=Flask(__name__)
+@app.route('/')
 def hello():
     return {"name":"SyOneitem2","description":"這是一個測試用的NFT，採用remix測試，檔案皆放在IPFS上，僅供測試。","image":"ipfs://Qmejnv4RrwviMaTA6LzWeNiWDTJ1y4e2Xm8N46BDYXvQGf","external_link":"https://www.google.com","attributes":[{"trait_type":"顏色","value":"綠"},{"trait_type":"文字","value":"SyOne"}]}
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port=80)
