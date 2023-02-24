b2ae2dac646a1640081aa1709316d4fd2d04508c
WINDOWS-P8I2MO6\syhuang <syhuang@cht.com.tw>
2023-02-24 13:32:25 +0800

    try http get image

--- hello-world.py
+++ hello-world.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return {"name":"SyOneitem2","description":"這是一個測試用的NFT，採用remix測試，檔案皆放在IPFS上，僅供測試。","image":"ipfs://Qmejnv4RrwviMaTA6LzWeNiWDTJ1y4e2Xm8N46BDYXvQGf","external_link":"https://www.google.com","attributes":[{"trait_type":"顏色","value":"綠"},{"trait_type":"文字","value":"SyOne"}]}
+    return {"name":"SyOneitem2","description":"這是一個測試用的NFT，採用remix測試，檔案皆放在IPFS上，僅供測試。","image":"https://web-eshop.cdn.hinet.net/eShop%20Images/other/404/EXPIRED.png","external_link":"https://www.google.com","attributes":[{"trait_type":"顏色","value":"綠"},{"trait_type":"文字","value":"SyOne"}]}
 
 if __name__ == '__main__':
     app.debug = True
